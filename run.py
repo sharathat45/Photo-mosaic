c0d7b3b8aeebc0027c82d8367bd33c22314be57e
SHARATH H S <46818446+sharathat45@users.noreply.github.com>
2021-05-31 03:14:31 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import subprocess
 # subprocess.run(["apt-get", "upgrade"])
 # subprocess.run(["apt-get", "update"])
-subprocess.run(["apt", "install", "libsm6"])
-subprocess.run(["apt", "install", "libxext6"])
-subprocess.run(["apt", "install", "ffmpeg"])
-subprocess.run(["apt", "install", "libfontconfig1"])
-subprocess.run(["apt", "install", "libxrender1"])
-subprocess.run(["apt", "install", "libgl1-mesa-glx"])
+# subprocess.run(["apt", "install", "libsm6"])
+# subprocess.run(["apt", "install", "libxext6"])
+# subprocess.run(["apt", "install", "ffmpeg"])
+# subprocess.run(["apt", "install", "libfontconfig1"])
+# subprocess.run(["apt", "install", "libxrender1"])
+# subprocess.run(["apt", "install", "libgl1-mesa-glx"])
 subprocess.run(["uvicorn", "server:app"])
 
