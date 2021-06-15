52bad9e3701387cec6d1981dd849307d2b730121
SHARATH H S <46818446+sharathat45@users.noreply.github.com>
2021-06-15 17:38:18 +0530

    Update mosaic.py

--- mosaic.py
+++ mosaic.py
@@ -4,14 +4,14 @@ from PIL import Image
 import os
 import io 
 import random
-from zipfile import ZipFile
+from zipfile import ZipFile,ZIP_DEFLATED
 import tempfile
 from google.cloud import storage
 
 def update_state(token,state, meta):
     client = storage.Client()
-    bucket = client.get_bucket('temp_files_mosaic')
-    
+    bucket = client.get_bucket(os.getenv('BUCKET_NAME',"temp_files_mosaic"))
+    print(state,meta['current'])
     try:
         temp = tempfile.NamedTemporaryFile() 
         iName = "".join([str(temp.name),".txt"])
@@ -25,15 +25,24 @@ def update_state(token,state, meta):
     except:
         pass
 
-    
-def get_mosaic(temp_folder_name:str, grayscale_flag: bool, focus_option:bool):
 
+def get_mosaic(temp_folder_name:str, grayscale_flag: bool, focus_option:bool):
+    try:
+        mosaic_task(temp_folder_name,grayscale_flag,focus_option)
+    except Exception as e:
+        print(e)
+        update_state(temp_folder_name,"ERROR",meta = {'current': 0})
+    return temp_folder_name    
+
+
+
+def mosaic_task(temp_folder_name:str, grayscale_flag: bool, focus_option:bool):
 #--------------------------Set variables of program--------------------------------------------------------
-    desired_height  = 20000  #15
-    desired_width  = 18000  #12          
+    desired_height  = int(os.getenv('IMAGE_HIGHT', 20000))
+    desired_width  = int(os.getenv('IMAGE_WIDTH', 18000))        
     grid_size = (36,18)
     storage_client = storage.Client()
-    bucket = storage_client.get_bucket("temp_files_mosaic")
+    bucket = storage_client.get_bucket(os.getenv('BUCKET_NAME',"temp_files_mosaic"))
     
     if focus_option:
         blend_factor = 0.8
@@ -44,9 +53,9 @@ def get_mosaic(temp_folder_name:str, grayscale_flag: bool, focus_option:bool):
 
 #------------------------------ Read target image and calcultae the shape--------------------------------------------------------
     target_img = 0
-    for blobs in bucket.list_blobs(prefix=temp_folder_name):
-        if (str(blobs)).find("target_img") != -1:
-            target_img = np.asarray(bytearray(blobs.download_as_string()),dtype="uint8")
+    for blob in bucket.list_blobs(prefix=temp_folder_name):
+        if (blob.name).find("target_img") != -1:
+            target_img = np.asarray(bytearray(blob.download_as_string()),dtype="uint8")
             target_img =cv2.imdecode(target_img,cv2.IMREAD_UNCHANGED)
             break
     
@@ -76,11 +85,12 @@ def get_mosaic(temp_folder_name:str, grayscale_flag: bool, focus_option:bool):
 
 #------------------------------ Read input images from zip file --------------------------------------------------------
     archive = 0
-    for blobs in bucket.list_blobs(prefix=temp_folder_name):
-            if str(blobs).find("temp_zip") != -1 :                
-                zipbytes = io.BytesIO(blobs.download_as_string())
-                archive = ZipFile(zipbytes, 'r')
-                break
+    for blob in bucket.list_blobs(prefix=temp_folder_name):
+        if str(blob.name).find("temp_zip") != -1 :                
+            zipbytes = io.BytesIO(blob.download_as_string())
+            archive = ZipFile(zipbytes, 'r')
+            break
+
     files = archive.namelist()
     random.shuffle(files)
 
@@ -127,8 +137,7 @@ def get_mosaic(temp_folder_name:str, grayscale_flag: bool, focus_option:bool):
             if (image_list_iter < n_store ):
                 images_list.append(input_img)
                 image_list_iter += 1
-                # update_state(state='PROGRESS', meta={'current': (40 + image_list_iter/10)})
-            
+                            
     archive.close()
     update_state(temp_folder_name,state='PROGRESS', meta={'current': 70})
 #------------------------------Using stored resized images to create final img--------------------------------------------------------
@@ -152,40 +161,31 @@ def get_mosaic(temp_folder_name:str, grayscale_flag: bool, focus_option:bool):
       if(x >= target_w):
         x = 0
         y += grid_h
-        # update_state(state='PROGRESS', meta={'current': 75+ iter/10})
-
+        
       if(y >= target_h):
         loop_condition = False  
         break
     
     update_state(temp_folder_name,state='PROGRESS', meta={'current': 85})    
 #------------------------------Save the final image--------------------------------------------------------
-    temp = tempfile.NamedTemporaryFile() 
-    iName = "".join([str(temp.name),".jpg"])
-    cv2.imwrite(iName,target_img)
-    blob = bucket.blob(temp_folder_name +'/PhotoMosaic.jpg')
-    blob.upload_from_filename(iName,content_type='image/jpeg')
-    temp.close()
+    archive = io.BytesIO()
+    with ZipFile(archive, 'w', compression = ZIP_DEFLATED) as zip_archive:
+        __, buf = cv2.imencode('.jpg', target_img)
+        zip_archive.writestr("Image.jpg", buf)
+    archive.seek(0)
+    blob = bucket.blob(temp_folder_name + '/PhotoMosaic.zip')
+    blob.upload_from_file(archive, content_type='application/zip')
+
 #------------------------------Delete input_imgs_zip file and target_img--------------------------------------------------------
     try:
-        for blobs in bucket.list_blobs(prefix=temp_folder_name):
-            blob_name = str(blobs)
-            if blob_name.find("target_img") != -1 or blob_name.find("temp_zip") != -1 :
-                blobs.delete() 
+        for blob in bucket.list_blobs(prefix=temp_folder_name):
+            blob_name = str(blob.name)
+            if blob_name.find("PhotoMosaic") == -1 and blob_name.find("status") == -1 :
+                blob.delete() 
     except :
         pass
 
     update_state(temp_folder_name,state='SUCCESS', meta={'current': 95})
 
 #------------------------------Return temp folder name--------------------------------------------------------
-    return temp_folder_name     
-   
-def remove_file(bucket_name, temp_folder_name):
-    try:
-        storage_client = storage.Client()
-        bucket = storage_client.get_bucket(bucket_name)
-        blobs = bucket.list_blobs(prefix=temp_folder_name)
-        for blob in blobs:
-            blob.delete()
-    except :
-        pass
+    return temp_folder_name        
