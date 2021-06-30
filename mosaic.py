405a4e445d3598c72c1e7a768ad7544d4dbdab6a
SHARATH H S <46818446+sharathat45@users.noreply.github.com>
2021-06-30 22:44:26 +0530

    Update mosaic.py

--- mosaic.py
+++ mosaic.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-from PIL import Image
+from PIL import Image, ImageOps
 import os
 import io 
 import random
@@ -9,10 +9,13 @@ import shutil
 from celery import Celery
 from dotenv import load_dotenv
 load_dotenv()
+
 broker_url = os.getenv('BROKER_URL') 
 backend_url = os.getenv('BACKEND_URL') 
 IMAGE_HIGHT = int(os.getenv('IMAGE_HIGHT'))
 IMAGE_WIDTH = int(os.getenv('IMAGE_WIDTH'))
+GRID_HIGHT = int(os.getenv('GRID_HIGHT'))
+GRID_WIDTH = int(os.getenv('GRID_WIDTH'))
 
 celery_app = Celery('tasks',
              broker= broker_url,
@@ -33,9 +36,11 @@ celery_app.conf.update(
 def get_mosaic(self, temp_folder_name:str, grayscale_flag: bool, focus_option:bool):
 
 #--------------------------Set variables of program--------------------------------------------------------
-    desired_height = IMAGE_HIGHT  #15
-    desired_width  = IMAGE_WIDTH  #12          
-    grid_size = (36,18)
+    desired_height = IMAGE_HIGHT  
+    desired_width  = IMAGE_WIDTH           
+    grid_w = GRID_WIDTH 
+    grid_h = GRID_HIGHT  #std height images are (400X400) or (700X400) or (aspect_ratio_maintained_width X 400)
+    ratio = (0.8,1.7)   #h/w ratio, if below lower threshold->landscape, above upper threshold->portrait, in between->square
     
     if focus_option:
         blend_factor = 0.8
@@ -55,109 +60,117 @@ def get_mosaic(self, temp_folder_name:str, grayscale_flag: bool, focus_option:bo
         target_img = cv2.imread(target_img_path)
         target_h,target_w = target_img.shape[:2] 
 
-    # resize_factor = 6
-    # target_h,target_w = target_h*resize_factor,target_w*resize_factor
-    if (target_h >= target_w):
-        hpercent = (desired_height/float(target_h))
-        target_w = int((float(target_w)*float(hpercent)))
-        target_h = desired_height
+    if(target_h/target_w  <= ratio[0]):  #if landscape image
+        desired_height = desired_width
+       
+    hpercent = (desired_height/float(target_h))
+    target_w = int((float(target_w)*float(hpercent)))
+    target_h = desired_height        
         
-        grid_h = target_h//max(grid_size)  
-        grid_w = target_w//min(grid_size) 
-    else:
-        wpercent = (desired_width/float(target_w))
-        target_h = int((float(target_h)*float(wpercent)))
-        target_w = desired_width
-          
-        grid_w = target_w//max(grid_size)  
-        grid_h = target_h//min(grid_size) 
-
     target_img = cv2.resize(target_img,(target_w, target_h), interpolation = cv2.INTER_CUBIC)
     self.update_state(state='PROGRESS', meta={'current': 20})
 
-#------------------------------ Read input images from zip file --------------------------------------------------------
+#------------------------------ Read input images from zip file and create mosaic--------------------------------------------------------
     input_imgs_path = os.path.join(temp_folder_path, "temp_zip.zip")
     archive = ZipFile(input_imgs_path, 'r')
     files = archive.namelist()
     random.shuffle(files)
     
-    images_list = [] 
-    image_list_iter = 0
-    
-    x=0 # width
-    y=0 # height
+    images_list = []     #store extracted images
+    images_list_iter = 0 #list iterator
+    x=0                  # width
+    y=0                  # height
+    i = 0                #while loop iterator
+    no_image_cnt = 0     #not images file count
+    n = len(files)       #images to store/shift operation
 
-    n_inputs = len(files)
-    n_required = grid_size[0]*grid_size[1] #648 #2592
-    n_store = n_required - n_inputs
-    n_store = 0 if n_store < 0 else n_store
-    self.update_state(state='PROGRESS', meta={'current': 40})
-
-#------------------------------ Resize input images , store and create final img--------------------------------------------------------
-    for image in files:
-        if image.split(".")[-1] in ["png","jpg","JPG","PNG"]:
-            imgdata = archive.read(image)
-            imgdata = io.BytesIO(imgdata)
-            imgdata = Image.open(imgdata)
-            imgdata.thumbnail((grid_w, grid_h))
-            
-            if grayscale_flag:
-                imgdata = imgdata.convert('L')
-                input_img = imgdata.resize((grid_w,grid_h), Image.LANCZOS)
-                input_img = np.array(input_img, np.uint8)                    
-            else:
-                imgdata = imgdata.convert('RGB')
-                input_img = imgdata.resize((grid_w,grid_h), Image.LANCZOS)
-                input_img = np.array(input_img, np.uint8)
-                input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB)
-            
-            try: 
-                target_img[y:y+grid_h, x:x+grid_w] = cv2.addWeighted(target_img[y:y+grid_h, x:x+grid_w],blend_factor,input_img,(1-blend_factor),0) 
-            except:
-                pass
-          
-            x += grid_w
-            if x >= target_w:
-                x = 0 
-                y += grid_h
+    while True:
+        if i<n:     #extract from zip,blend and store in list
+            if files[i].split(".")[-1] in ["png","jpg","JPG","PNG"]:   
+                imgdata = archive.read(files[i])
+                imgdata = io.BytesIO(imgdata)
+                imgdata = Image.open(imgdata)
+                imgdata = ImageOps.exif_transpose(imgdata) #ignore metadata orientation
+                imgdata.thumbnail((grid_w, grid_h))
                 
-            if (image_list_iter < n_store ):
+                if grayscale_flag:
+                    imgdata = imgdata.convert('L')
+                else:
+                    imgdata = imgdata.convert('RGB')
+
+                w,h = imgdata.size
+                if(h/w >= ratio[1]):                                           #if portrait image
+                    input_img = imgdata
+                elif(h/w <= ratio[0]):                                         #if landscape image
+                    input_img = imgdata.resize((grid_w,grid_h), Image.LANCZOS)
+                    w,h = grid_w,grid_h
+                else:                                                           #if square image
+                    input_img = imgdata.resize((grid_h,grid_h), Image.LANCZOS)
+                    w,h = grid_h,grid_h
+                
+                input_img = np.array(input_img, np.uint8)
+                if grayscale_flag==False:
+                    input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB)
+
                 images_list.append(input_img)
-                image_list_iter += 1
-                self.update_state(state='PROGRESS', meta={'current': (40 + image_list_iter/10)})
+            else: #if not a image file
+                no_image_cnt +=1
+                if no_image_cnt == n-1: #if zip file dosen't contain images
+                    break
+                i+=1
+                continue    
+            i+=1  
+            self.update_state(state='PROGRESS', meta={'current': 20 + round(i/10)})   
+        elif i==n: # shuffle images list after extraction from zip
+            archive.close() 
+            i+=1
+            random.shuffle(images_list)
+            n = n-no_image_cnt
+            input_img = images_list[images_list_iter]
+            h,w = input_img.shape[:2]
+            images_list_iter +=1
+            self.update_state(state='PROGRESS', meta={'current': 60})   
             
-    archive.close()
-    self.update_state(state='PROGRESS', meta={'current': 70})
-#------------------------------Using stored resized images to create final img--------------------------------------------------------
-    random.shuffle(images_list)
-    iter = 0
-    loop_condition = True
-    iter_limit = len(images_list)
-    while loop_condition:
-          
-      if (iter >= iter_limit):
-        random.shuffle(images_list)
-        iter = 0   
-
-      try:
-        target_img[y:y+grid_h, x:x+grid_w] = cv2.addWeighted(target_img[y:y+grid_h, x:x+grid_w],blend_factor,images_list[iter],(1-blend_factor),0) 
-        iter = iter + 1
-      except:
-          pass
+        else: # get the images from image list
+            if images_list_iter > n-1:
+                random.shuffle(images_list)
+                images_list_iter = 0 
+            
+            input_img = images_list[images_list_iter]
+            h,w = input_img.shape[:2]
+            images_list_iter += 1
      
-      x += grid_w
-      if(x >= target_w):
-        x = 0
-        y += grid_h
-        self.update_state(state='PROGRESS', meta={'current': 75+ iter/10})
-
-      if(y >= target_h):
-        loop_condition = False  
-        break
+        if x+w > target_w:  # x1+x2 = w , x1 -> percent of img inside target_img frame (for edge background images)
+            x1_percent = (target_w-x)/w                
+            if x1_percent <= 0.3:  # very small percent is inside then ignore and leave it empty space
+                x=0
+                y += h
+                if y >= target_h:
+                    break
+            else: # most percent is inside or outside then expand current img and fit to frame 
+                w = target_w-x
+                try: 
+                    input_img = cv2.resize(input_img,(w,grid_h), interpolation = cv2.INTER_LINEAR)
+                    target_img[y:y+h, x:x+w] = cv2.addWeighted(target_img[y:y+h, x:x+w],blend_factor,input_img,(1-blend_factor),0) 
+                except:
+                    pass
+                x=0
+                y += h
+                if y >= target_h:
+                    break 
+                else:
+                    continue
+        try: 
+            target_img[y:y+h, x:x+w] = cv2.addWeighted(target_img[y:y+h, x:x+w],blend_factor,input_img,(1-blend_factor),0)  
+        except:
+            pass
+        x += w 
+               
     
+    self.update_state(state='PROGRESS', meta={'current': 80})    
 #------------------------------Save the final image--------------------------------------------------------
     cv2.imwrite(os.path.join(temp_folder_path,"PhotoMosaic.jpg"),target_img) 
-
+    self.update_state(state='PROGRESS', meta={'current': 90})    
 #------------------------------Delete input_imgs_zip file and target_img--------------------------------------------------------
     try:
         os.remove(target_img_path)
